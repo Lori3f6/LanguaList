67dc2062a4c1b1bc2cbb8d4b94786fb3453159dd
Lori3f6 <55254968+Lori3f6@users.noreply.github.com>
2019-09-21 23:50:42 +0800

    From 0 to 1 	new file:   pom.xml 	new file:   src/main/java/moe/langua/lab/Langualist.java

---
+++ src/main/java/moe/langua/lab/Langualist.java
@@ -0,0 +1,100 @@
+package moe.langua.lab;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Langualist<T> implements List<T> {
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    public boolean add(T t) {
+        return false;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T set(int index, T element) {
+        return null;
+    }
+
+    public void add(int index, T element) {
+
+    }
+
+    public T remove(int index) {
+        return null;
+    }
+
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
